08e6ff2dec15f1aaaad9e1fe9ea292c563b1878e
Vadim <cloudplaid@yandex.ru>
2021-03-23 17:11:08 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+   <table>
+     <tr v-for="(item, idx) in testData" :key="idx">
+       <td>{{ item.title }}</td>
+       <td>{{ item.qty }}</td>
+     </tr>
+   </table>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      testData: null
+    }
+  },
+  created() {
+    fetch("http://localhost:1337/items")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        this.testData = data
+      });
+  }
+}
+</script>
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ac091d1-70b0-4336-9cf8-47a4e5e7dc52'
+};
